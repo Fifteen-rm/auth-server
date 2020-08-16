6e1a235074b7fca6ba70af9163adc66d9564f379
key kim <keyhyuk.kim@gmail.com>
2020-08-17 01:33:30 +0900

    redis initial commit

--- authServer.js
+++ authServer.js
@@ -1,49 +1,87 @@
-  
-require('dotenv').config()
+import {profiles} from './models/model.js'
+import { promisify } from 'util'
 
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
+const redis = require("redis");
+
+var rediscl = redis.createClient();
+rediscl.on("connect",  () => {
+    console.log("Redis plugged in.");
+});
 
 app.use(express.json())
-
-let refreshTokens = [] // use 
+ 
+const getAsync = promisify(rediscl.get).bind(rediscl)
 
 app.post('/token', (req, res) => {
   const refreshToken = req.body.token
   if (refreshToken == null) 
     return res.sendStatus(401)
   
-  if (!refreshTokens.includes(refreshToken)) 
-    return res.sendStatus(403)
-  
+    
   jwt.verify(refreshToken, process.env.SECRET_REFRESH_TOKEN, (err, user) => {
     if (err) 
       return res.sendStatus(403)
+
+    console.log(user)
+    console.log(refreshToken)
+
+    getAsync(user).then((res) => {
+      
+    })
+
     const accessToken = generateAccessToken({ name: user.name })
+
     res.json({ accessToken: accessToken })
   })
 })
 
+
+// TO DO : refactor using async await  | by kimkihyuk
 app.delete('/logout', (req, res) => {
-  refreshTokens = refreshTokens.filter(token => token !== req.body.token)
-  res.sendStatus(204)
+  jwt.verify(token, process.env.SECRET_REFRESH_TOKEN, (err, user) => {
+    console.log('[logout] user is ', user)
+    if (err)
+      return res.send(403, {response: `you are not ${user}`})
+    req.user = user
+    
+    rediscl.del(req.user)
+
+    res.sendStatus(204)
+  })
 })
 
 app.post('/login', (req, res) => {
-  // Authenticate User
-
   const username = req.body.username
   const user = { name: username }
 
+  if (model.profiles.some(profile => profile.username !== user.name)) {
+    return res.sendStatus(401)
+  }
+
   const accessToken = generateAccessToken(user)
-  const refreshToken = jwt.sign(user, process.env.SECRET_REFRESH_TOKEN)
-  refreshTokens.push(refreshToken)
+  const refreshToken = jwt.sign(user, process.env.SECRET_REFRESH_TOKEN, {expiresIn: '1h'})
+
+  res.cookie('access_token', accessToken, {
+    httpOnly: true
+  })
+  res.cookie('refresh_token', refreshToken, {
+    httpOnly: true
+  })
+
+  rediscl.set(user.name, JSON.stringify({
+    refresh_token: refreshToken,
+    expires: new Date() + 60 * 60 * 1 // 1hour
+  }))
+
   res.json({ accessToken: accessToken, refreshToken: refreshToken })
 })
 
 const generateAccessToken = (user) => {
-  return jwt.sign(user, process.env.SECRET_ACCESS_TOKEN, { expiresIn: '10s' })
+  return jwt.sign(user, process.env.SECRET_ACCESS_TOKEN, { expiresIn: '1m' })
 }
 
 app.listen(4000)
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 require('dotenv').config()
 
+import {profiles} from "models/model"
+
+console.log(profiles)
+// TO DO : require should be replaced import | kimkihyuk
+const redis = require("redis");
+var rediscl = redis.createClient();
+
+rediscl.on("connect", () => {
+  console.log("Redis plugged in.");
+});
+
 const express = require('express')
 const app = express()
-
 const jwt = require('jsonwebtoken')
-app.use(express.json())
 
-const posts = [
-    {
-      username: 'Kyle',
-      title: 'Post 1'
-    },
-    {
-      username: 'Jim',
-      title: 'Post 2'
-    }
-  ]
+app.use(express.json())
 
 
 const authenticateToken = (req, res, next) => {
@@ -26,6 +26,7 @@ const authenticateToken = (req, res, next) => {
       return res.sendStatus(401)
   
     jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, (err, user) => {
+      console.log('[authenticate] user is ', user)
       if (err)
         return res.sendStatus(403)
       req.user = user
@@ -34,9 +35,7 @@ const authenticateToken = (req, res, next) => {
   }
   
 app.get('/posts', authenticateToken, (req, res) => {
-  res.json(posts.filter(post => post.username === req.user.name))
+  res.json(profiles.filter(profile => profile.username === req.user.name))
 })
-
-
 
 app.listen(3000)
