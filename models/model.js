5e96bca6469b026a53c1f95044649e171ace3096
key kim <keyhyuk.kim@gmail.com>
2020-08-19 01:55:44 +0900

    Add model

---
+++ models/model.js
@@ -0,0 +1,15 @@
+// refactor fetching data from database | kimkihyuk
+export var profiles = [
+    { // test 계정
+      username: 'test',
+      id: '960502-*******'
+    },
+    {
+      username: 'user1',
+      id: '970812-*******'
+    },
+    {
+      username: 'user2',
+      id: '930813-*******'
+    },
+  ]
